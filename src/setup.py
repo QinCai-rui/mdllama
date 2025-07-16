3667ed2a1982342525b4b0f7b0753933e3387e31
Raymont Qin <140027854+QinCai-rui@users.noreply.github.com>
2025-07-17 07:53:46 +1200

    Update setup.py

--- src/setup.py
+++ src/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="mdllama",
-    version="20250716.1",
+    version="20250716.7",
     description="A command-line interface for Ollama API",
     author="QinCai-rui",
     packages=find_packages(),
