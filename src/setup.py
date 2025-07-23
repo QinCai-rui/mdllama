07fe2afa801a0c6bcac5cf7fa4fd6e14dc90d967
QinCai-rui <140027854+QinCai-rui@users.noreply.github.com>
2025-07-23 01:51:52 +0000

    chore(beta): update RELEASE and version for beta/testing push [Actions]

--- src/setup.py
+++ src/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="mdllama",
-    version="20250718.3",
+    version="20250723.1",
     description="A command-line interface for Ollama API",
     author="QinCai-rui",
     packages=find_packages(),
